f8bca9d0d51bdc46d7cdcfa8fcacc2f029088d49
Tara Baskara <tara.baskara@gmail.com>
2018-04-18 13:55:51 +0700

    Added deploy a single web server code

---
+++ chapter02-getting-started-with-terraform/deploy-a-single-web-server/main.tf
@@ -0,0 +1,31 @@
+provider "aws" {
+  region     = "${var.region}"
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+}
+
+resource "aws_instance" "example" {
+  ami                     = "ami-52d4802e"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = ["${aws_security_group.instance.id}"] 
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello world" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+  tags {
+    Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
