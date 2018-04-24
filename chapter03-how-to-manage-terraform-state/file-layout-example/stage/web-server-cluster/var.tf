a2ffb4dbe7c73fe8cbdebf3f6b419eed017ac0ca
Tara Baskara <tara.baskara@gmail.com>
2018-04-24 11:04:24 +0700

    Updated web-server code

---
+++ chapter03-how-to-manage-terraform-state/file-layout-example/stage/web-server-cluster/var.tf
@@ -0,0 +1,28 @@
+variable "aws_access_key" { }
+variable "aws_secret_key" { }
+
+variable "region" {
+  default = "ap-southeast-1"
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default     = 8080
+}
+
+variable "list_example" {
+  description = "An example of a list in Terraform"
+  type        = "list"
+  default     = [1, 2, 3]
+}
+
+variable "map_example" {
+  description = "An example of a map in Terraform"
+  type        = "map"
+
+  default = {
+    key1 = "value1"
+    key2 = "value2"
+    key3 = "value3"
+  }
+}
